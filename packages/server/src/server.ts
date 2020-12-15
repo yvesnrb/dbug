92947f671f4ea4186359b08630dff57a3c937846
Yves Bode <ynrbode@gmail.com>
2020-12-15 16:49:38 -0300

    ci(global): this is a test of our CI setup
    This test is to verify that the application is changing on a commit to it's master branch of GitHub.

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_request, response) => {
-  return response.json({ message: 'Hello, Digital Ocean Apps!' });
+  return response.json({ message: 'Hello from Digital Ocean CI solution!' });
 });
 
 app.listen(process.env.APP_PORT, () => {
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -4,6 +4,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Hello from dbug!</h1>
+      <p>Hello from Digital Ocean CI solution!</p>
     </div>
   );
 };
