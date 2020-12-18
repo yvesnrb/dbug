e88d254a0018bc7f63695e715232840652e246be
Yves Bode <ynrbode@gmail.com>
2020-12-18 14:47:12 -0300

    test(@dbug/web): modifies Button's stories to use strings as children instead of fragments

--- packages/web/src/components/Button/stories.tsx
+++ packages/web/src/components/Button/stories.tsx
@@ -16,32 +16,32 @@ const Template: Story<ButtonProps> = args => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   color: 'primary',
-  children: <>Im a button!</>,
+  children: 'Im a button!',
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   color: 'primary',
   disabled: true,
-  children: <>Im a button!</>,
+  children: 'Im a button!',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   color: 'secondary',
-  children: <>Im a button!</>,
+  children: 'Im a button!',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   color: 'primary',
   small: true,
-  children: <>Im a button!</>,
+  children: 'Im a button!',
 };
 
 export const Loading = Template.bind({});
 Loading.args = {
   color: 'primary',
   loading: true,
-  children: <>Im a button!</>,
+  children: 'Im a button!',
 };
