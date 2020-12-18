093e75831cc90c83e884d00e822cda41d4870c2f
Yves Bode <ynrbode@gmail.com>
2020-12-18 11:12:07 -0300

    feat(@dbug/web): implemented a global decorator for storybook
    This global decorator provides the application's main theme to all stories.

--- packages/web/.storybook/preview.js
+++ packages/web/.storybook/preview.js
@@ -1,4 +1,16 @@
+import React from "react";
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/appTheme';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
