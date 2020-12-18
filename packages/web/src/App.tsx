6f77b88ddec8e7dff3c04dee46d0ac0261319415
Yves Bode <ynrbode@gmail.com>
2020-12-18 14:29:11 -0300

    feat(@dbug/web): implemented app's theme and global stylesheet into main App component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './globalStyle';
+import theme from './appTheme';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello from dbug!</h1>
-      <p>Hello from Digital Ocean CI solution!</p>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <h1>Hello from dbug!</h1>
+        <p>Hello from Digital Ocean CI solution!</p>
+      </ThemeProvider>
+    </>
   );
 };
 
