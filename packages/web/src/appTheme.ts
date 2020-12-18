7b0b62ca6d652b9b9606bf5191a91072c332c05b
Yves Bode <ynrbode@gmail.com>
2020-12-18 14:28:00 -0300

    feat(@dbug/web): implemented font families in app's theme

--- packages/web/src/appTheme.ts
+++ packages/web/src/appTheme.ts
@@ -4,4 +4,13 @@ export default {
   background: '#424242',
   foreground: '#F2F2F2',
   danger: '#FF3636',
+  displayFont: '700 4.8rem Roboto',
+  copyFont: '400 1.8rem Roboto',
+  copySmallFont: '400 1.6rem Roboto',
+  copyLargeFont: '400 2.5rem Roboto',
+  copyBoldFont: '700 1.6rem Roboto',
+  buttonFont: '700 1.8rem Roboto',
+  buttonSmallFont: '700 1.6rem Roboto',
+  helpFont: '400 1rem Roboto',
+  helpLargeFont: '400 1.2rem Roboto',
 };
