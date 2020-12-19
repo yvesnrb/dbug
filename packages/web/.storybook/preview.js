90cfd12a3430e4eb94721de44e7ce69171219a25
Yves Bode <ynrbode@gmail.com>
2020-12-19 19:32:42 -0300

    feat(@dbug/web): added breakpoints to storybook's manager panel

--- packages/web/.storybook/preview.js
+++ packages/web/.storybook/preview.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/appTheme';
 import { GlobalStyle } from '../src/globalStyle';
 
 export const decorators = [
-  (Story) => (
+  Story => (
     <ThemeProvider theme={theme}>
       <Router>
         <GlobalStyle />
@@ -16,6 +16,44 @@ export const decorators = [
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
-
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      small: {
+        name: 'Small',
+        styles: {
+          width: '576px',
+          height: '800px',
+        },
+      },
+      medium: {
+        name: 'Medium',
+        styles: {
+          width: '768px',
+          height: '900px',
+        },
+      },
+      large: {
+        name: 'Large',
+        styles: {
+          width: '992px',
+          height: '720px',
+        },
+      },
+      xLarge: {
+        name: 'Extra Large',
+        styles: {
+          width: '1200px',
+          height: '900px',
+        },
+      },
+      xxLarge: {
+        name: 'Extra Extra Large',
+        styles: {
+          width: '1400px',
+          height: '1000px',
+        },
+      },
+    },
+  },
+};
