bb4bf57c21adc12a1fae6dd396bad9358cae9979
Yves Bode <ynrbode@gmail.com>
2020-12-22 20:45:36 -0300

    feat(@dbug/server): implemented sessions router

--- packages/server/src/routes/sessions-route.ts
+++ packages/server/src/routes/sessions-route.ts
@@ -1,41 +1,14 @@
 import { Router } from 'express';
-import axios from 'axios';
+import AuthenticateUserService from '../services/authenticate-user-service';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   const { code } = request.body;
+  const authenticateUserService = new AuthenticateUserService(code);
+  const credentials = await authenticateUserService.execute();
 
-  try {
-    const oauthResponse = await axios.post(
-      'https://github.com/login/oauth/access_token',
-      {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
-      },
-      {
-        headers: {
-          accept: 'application/json',
-        },
-      },
-    );
-
-    console.log(oauthResponse.data);
-
-    const userResponse = await axios.get('https://api.github.com/user', {
-      headers: {
-        accept: 'application/json',
-        Authorization: `token ${oauthResponse.data.access_token}`,
-      },
-    });
-
-    console.log(userResponse.data);
-
-    return response.json(userResponse.data);
-  } catch (err) {
-    return response.json({ status: 'err' });
-  }
+  return response.json(credentials);
 });
 
 export default sessionsRouter;
