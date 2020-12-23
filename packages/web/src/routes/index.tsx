d9cfd63bc656fc591dc72c68a068b821fa2c6441
Yves Bode <ynrbode@gmail.com>
2020-12-23 17:49:02 -0300

    feat(@dbug/web): added the Dashboard component to main router

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
