6e51d7d9f0936a096099966a59d6053dabfe4460
Yves Bode <ynrbode@gmail.com>
2020-12-23 17:53:24 -0300

    feat(@dbug/web): added the signout page to the main router

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -3,12 +3,14 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
+import SignOut from '../pages/SignOut';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
+      <Route exact path="/signout" component={SignOut} isPrivate />
     </Switch>
   );
 };
