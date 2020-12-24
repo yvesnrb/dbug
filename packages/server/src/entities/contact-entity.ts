ee97e0ca330fd57527184fa9cf4513cc4678b9c6
Yves Bode <ynrbode@gmail.com>
2020-12-24 16:59:27 -0300

    feat(@dbug/server): created contact entity

---
+++ packages/server/src/entities/contact-entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('contacts')
+export default class Contact {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: true })
+  meet: string;
+
+  @Column({ nullable: true })
+  discord: string;
+
+  @Column({ nullable: true })
+  zoom: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
