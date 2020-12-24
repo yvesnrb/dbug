42d4843a599baeb74607f64e161d5755aaf2c707
Yves Bode <ynrbode@gmail.com>
2020-12-24 16:57:57 -0300

    feat(@dbug/server): implemented create contacts table migration

---
+++ packages/server/src/database/migrations/1608831722141-create-contacts-table.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class createContactsTable1608831722141
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'contacts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'meet',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'discord',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'zoom',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('contacts');
+  }
+}
