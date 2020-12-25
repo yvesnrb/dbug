79a370b6c3b6c2c264cc3f51fb3c16b2bf823958
Yves Bode <ynrbode@gmail.com>
2020-12-25 10:43:09 -0300

    feat(@dbug/server): added the contacts router to the index router of the application

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import contactsRouter from './contacts-route';
 import sessionsRouter from './sessions-route';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
+routes.use('/contacts', contactsRouter);
 
 export default routes;
