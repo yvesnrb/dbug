f8111267d966f88980b9630be4da307df5f6b17c
Yves Bode <ynrbode@gmail.com>
2020-12-27 19:41:18 -0300

    feat(@dbug/server): implemented contact schema validation in contacts route

--- packages/server/src/routes/contacts-route.ts
+++ packages/server/src/routes/contacts-route.ts
@@ -1,40 +1,52 @@
 import { Router } from 'express';
+import { validate } from 'express-validation';
 import CreateContactService from '../services/create-contact-service';
 import authorizeToken from '../middleware/authorize-token-middleware';
 import UpdateContactService from '../services/update-contact-service';
+import contactSchema from '../schemas/contact-schema';
 
 const contactsRouter = Router();
 
-contactsRouter.post('/', authorizeToken, async (request, response) => {
-  const { meet, discord, zoom } = request.body;
-  const createContactService = new CreateContactService({
-    userId: request.session.id,
-    contactInfo: {
-      meet,
-      discord,
-      zoom,
-    },
-  });
+contactsRouter.post(
+  '/',
+  authorizeToken,
+  validate({ body: contactSchema }),
+  async (request, response) => {
+    const { meet, discord, zoom } = request.body;
+    const createContactService = new CreateContactService({
+      userId: request.session.id,
+      contactInfo: {
+        meet,
+        discord,
+        zoom,
+      },
+    });
 
-  const contact = await createContactService.execute();
+    const contact = await createContactService.execute();
 
-  return response.status(201).json(contact);
-});
+    return response.status(201).json(contact);
+  },
+);
 
-contactsRouter.put('/', authorizeToken, async (request, response) => {
-  const { meet, discord, zoom } = request.body;
-  const updateContactService = new UpdateContactService({
-    userId: request.session.id,
-    contactInfo: {
-      meet,
-      discord,
-      zoom,
-    },
-  });
+contactsRouter.put(
+  '/',
+  authorizeToken,
+  validate({ body: contactSchema }),
+  async (request, response) => {
+    const { meet, discord, zoom } = request.body;
+    const updateContactService = new UpdateContactService({
+      userId: request.session.id,
+      contactInfo: {
+        meet,
+        discord,
+        zoom,
+      },
+    });
 
-  const contact = await updateContactService.execute();
+    const contact = await updateContactService.execute();
 
-  return response.status(200).json(contact);
-});
+    return response.status(200).json(contact);
+  },
+);
 
 export default contactsRouter;
