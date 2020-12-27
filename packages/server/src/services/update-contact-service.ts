a33c12a1febab080e0a474111c42ef1af5dcfa1c
Yves Bode <ynrbode@gmail.com>
2020-12-27 18:35:50 -0300

    feat(@dbug/server): implemented update contact service

---
+++ packages/server/src/services/update-contact-service.ts
@@ -0,0 +1,71 @@
+import { getRepository } from 'typeorm';
+import Contact from '../entities/contact-entity';
+import User from '../entities/user-entity';
+import defineAbilitiesFor from '../abilities/user-abilities';
+import AppError from '../errors/app-error';
+
+interface Request {
+  userId: string;
+  contactInfo: {
+    meet?: string;
+    discord?: string;
+    zoom?: string;
+  };
+}
+
+class UpdateContactService {
+  private userId: string;
+
+  private contactInfo: Request['contactInfo'];
+
+  private contactsRepository = getRepository(Contact);
+
+  private usersRepository = getRepository(User);
+
+  private user: User;
+
+  private contact: Contact;
+
+  constructor(data: Request) {
+    this.userId = data.userId;
+    this.contactInfo = data.contactInfo;
+  }
+
+  public async execute(): Promise<Contact> {
+    await this.fetchUser();
+    this.verifyAbility();
+    await this.fetchContact();
+    await this.updateContact();
+
+    return this.contact;
+  }
+
+  public async fetchUser(): Promise<void> {
+    this.user = await this.usersRepository.findOneOrFail({
+      where: { id: this.userId },
+    });
+  }
+
+  public async fetchContact(): Promise<void> {
+    this.contact = await this.contactsRepository.findOneOrFail({
+      where: { id: this.user.contact_id },
+    });
+  }
+
+  public async updateContact(): Promise<void> {
+    this.contact.meet = this.contactInfo.meet;
+    this.contact.discord = this.contactInfo.discord;
+    this.contact.zoom = this.contactInfo.zoom;
+
+    await this.contactsRepository.save(this.contact);
+  }
+
+  public verifyAbility(): void {
+    const ability = defineAbilitiesFor(this.user);
+
+    if (ability.cannot('update', 'Contact'))
+      throw new AppError('badrequest', 400);
+  }
+}
+
+export default UpdateContactService;
