de7d632bdf8c90beac50a89e233d3666c0633348
Yves Bode <ynrbode@gmail.com>
2020-12-27 19:45:24 -0300

    feat(@dbug/server): implemented session schema in sessions router

--- packages/server/src/routes/sessions-route.ts
+++ packages/server/src/routes/sessions-route.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
+import { validate } from 'express-validation';
 import AuthenticateUserService from '../services/authenticate-user-service';
+import sessionSchema from '../schemas/session-schema';
 
 const sessionsRouter = Router();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { code } = request.body;
-  const authenticateUserService = new AuthenticateUserService(code);
-  const credentials = await authenticateUserService.execute();
+sessionsRouter.post(
+  '/',
+  validate({ body: sessionSchema }),
+  async (request, response) => {
+    const { code } = request.body;
+    const authenticateUserService = new AuthenticateUserService(code);
+    const credentials = await authenticateUserService.execute();
 
-  return response.json(credentials);
-});
+    return response.json(credentials);
+  },
+);
 
 export default sessionsRouter;
