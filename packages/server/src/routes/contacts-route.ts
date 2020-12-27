c08029d7bcb2a1820807417fc85d332d720d7a98
Yves Bode <ynrbode@gmail.com>
2020-12-27 18:36:19 -0300

    feat(@dbug/server): implemented PUT method in contacts route

--- packages/server/src/routes/contacts-route.ts
+++ packages/server/src/routes/contacts-route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import CreateContactService from '../services/create-contact-service';
 import authorizeToken from '../middleware/authorize-token-middleware';
+import UpdateContactService from '../services/update-contact-service';
 
 const contactsRouter = Router();
 
@@ -18,6 +19,22 @@ contactsRouter.post('/', authorizeToken, async (request, response) => {
   const contact = await createContactService.execute();
 
   return response.status(201).json(contact);
+});
+
+contactsRouter.put('/', authorizeToken, async (request, response) => {
+  const { meet, discord, zoom } = request.body;
+  const updateContactService = new UpdateContactService({
+    userId: request.session.id,
+    contactInfo: {
+      meet,
+      discord,
+      zoom,
+    },
+  });
+
+  const contact = await updateContactService.execute();
+
+  return response.status(200).json(contact);
 });
 
 export default contactsRouter;
