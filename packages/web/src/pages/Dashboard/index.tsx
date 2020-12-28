3a28cf105df007420f605776093d9202ad79b063
Yves Bode <ynrbode@gmail.com>
2020-12-28 12:34:09 -0300

    feat(@dbug/web): implemented a redirect in Dashboard for users that do not have contact info

--- packages/web/src/pages/Dashboard/index.tsx
+++ packages/web/src/pages/Dashboard/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import MainNav from '../../components/MainNav';
+import { useAuth } from '../../context/AuthContext';
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+  const {
+    data: { user },
+  } = useAuth();
+
   return (
     <>
+      {!user.contact_id ? <Redirect to="/contact" /> : null}
+
       <MainNav icons />
       <Container>
         <p>this page will be the dashboard very shortly!</p>
