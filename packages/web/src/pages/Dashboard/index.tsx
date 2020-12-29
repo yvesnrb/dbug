043913ed2e12c020382b07f011acb21a5f73c114
Yves Bode <ynrbode@gmail.com>
2020-12-29 14:55:27 -0300

    feat(@dbug/web): implemented header component in dashboard page

--- packages/web/src/pages/Dashboard/index.tsx
+++ packages/web/src/pages/Dashboard/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FiPlusCircle } from 'react-icons/fi';
 import { Redirect } from 'react-router-dom';
+import Badge from '../../components/Badge';
+import { LinkButton } from '../../components/Button';
 import MainNav from '../../components/MainNav';
 import { useAuth } from '../../context/AuthContext';
-import { Container } from './styles';
+import { Header } from './styles';
 
 const Dashboard: React.FC = () => {
   const {
@@ -14,10 +17,49 @@ const Dashboard: React.FC = () => {
       {!user.contact_id ? <Redirect to="/contact" /> : null}
 
       <MainNav icons />
-      <Container>
-        <p>this page will be the dashboard very shortly!</p>
-        <p>thanks for testing my application :)</p>
-      </Container>
+      <Header>
+        <div className="header__content">
+          <div className="header__userinfo">
+            <img src={user.avatar_url} alt={`${user.login} avatar`} />
+
+            <div>
+              <h2>{user.login}</h2>
+
+              <Badge
+                color="#0A897B"
+                className="header__badge"
+                disabled={!user.contact?.meet}
+              >
+                Meet
+              </Badge>
+
+              <Badge
+                color="#8A9CFA"
+                className="header__badge"
+                disabled={!user.contact?.discord}
+              >
+                Discord
+              </Badge>
+
+              <Badge
+                color="#2FA2FB"
+                className="header__badge"
+                disabled={!user.contact?.zoom}
+              >
+                Zoom
+              </Badge>
+            </div>
+          </div>
+
+          <LinkButton
+            className="header__button"
+            to="newproject"
+            color="primary"
+          >
+            Post a Project <FiPlusCircle size={18} />
+          </LinkButton>
+        </div>
+      </Header>
     </>
   );
 };
--- packages/web/src/pages/Dashboard/styles.ts
+++ packages/web/src/pages/Dashboard/styles.ts
@@ -1,11 +1,52 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  max-width: ${props => props.theme.siteWidth};
-  padding: 2rem 2rem;
-  margin: 0 auto;
+export const Header = styled.div`
+  background: ${props => props.theme.secondary};
 
-  p {
-    font: ${props => props.theme.copyFont};
+  .header__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: ${props => props.theme.siteWidth};
+    padding: 2rem 2rem;
+    margin: 0 auto;
+  }
+
+  .header__userinfo {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 9rem;
+      height: auto;
+      border-radius: 50%;
+      margin-right: 1.5rem;
+    }
+
+    h2 {
+      font: ${props => props.theme.copyLargeFont};
+      margin-bottom: 1rem;
+    }
+  }
+
+  .header__badge {
+    & + .header__badge {
+      margin-left: 1rem;
+    }
+  }
+
+  @media only screen and (max-width: ${props => props.theme.smallBreakpoint}) {
+    .header__content {
+      align-items: flex-start;
+      flex-direction: column;
+    }
+
+    .header__userinfo {
+      margin-bottom: 1.5rem;
+    }
+
+    .header__button {
+      align-self: center;
+    }
   }
 `;
