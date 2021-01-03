66444571e96ef27584a5be41eeb88e916ff9ea22
Yves Bode <ynrbode@gmail.com>
2021-01-03 00:01:48 -0300

    feat(@dbug/server): implemented POST /projects route

--- packages/server/src/routes/projects-route.ts
+++ packages/server/src/routes/projects-route.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import authorizeTokenMiddleware from '../middleware/authorize-token-middleware';
+import CreateProjectService from '../services/create-project-service';
 
 const projectsRouter = Router();
 
@@ -10,9 +12,21 @@ projectsRouter.get('/:id', async (_request, response) => {
   return response.json({ message: 'finding project...' });
 });
 
-projectsRouter.post('/', async (_request, response) => {
-  return response.json({ message: 'creating project...' });
-});
+projectsRouter.post(
+  '/',
+  authorizeTokenMiddleware,
+  async (request, response) => {
+    const {
+      body: { body },
+      session: { id },
+    } = request;
+    const createProjectService = new CreateProjectService({ body, userId: id });
+
+    const project = await createProjectService.execute();
+
+    return response.json(project);
+  },
+);
 
 projectsRouter.post('/:id/share', async (_request, response) => {
   return response.json({ message: 'sharing with project...' });
